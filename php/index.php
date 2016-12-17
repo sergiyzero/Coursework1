239033144ad586d0527796e95f3cf872a48276c3
sergiyzero <sergobala@mail.ru>
2016-12-17 14:26:30 +0200

    initial commit

---
+++ php/index.php
@@ -0,0 +1,22 @@
+
+<title>Автоновости</title>
+
+<?php
+
+include_once('setting.php');
+
+$connection = mysqli_connect(HOST, USER, PASSWORD, DB);
+
+if(!$connection) exit("Ошыбка подключения к БД!");
+
+$query = mysqli_query ( $connection,  'SELECT * FROM `auto` ' );
+
+while ($row = mysqli_fetch_assoc($query))
+
+{
+
+    echo "<br>$row[title]";
+    echo "<br>$row[text]";
+}
+
+?>
---
+++ php/setting.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: сергей
+ * Date: 14.12.2016
+ * Time: 20:00
+ */
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', '');
+define('DB', 'my_bd');
